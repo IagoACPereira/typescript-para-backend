63842f87ed16673f6fa283e536c3bd31a94153d7
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-08-26 08:08:06 -0300

    feat: :sparkles: add: Aplicando o TipoPet no CRUD

--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -25,4 +25,43 @@ export default class PetController{
 
     res.status(201).json(novoPet);
   }
+
+  listaPet(req: Request, res: Response) {
+    return res.status(200).json(listaDePets);
+  }
+
+  atualizaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const {
+      adotado,
+      especie,
+      idade,
+      nome,
+    } = req.body as TipoPet;
+    const pet = listaDePets.find(pet => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: 'Pet não encontrado' })
+    }
+
+    pet.nome = nome;
+    pet.idade = idade;
+    pet.especie = especie;
+    pet.adotado = adotado;
+
+    return res.status(200).json(pet);
+  }
+
+  deletaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const pet = listaDePets.find(pet => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: 'Pet não encontrado' })
+    }
+
+    const index = listaDePets.indexOf(pet);
+    listaDePets.splice(index, 1);
+    return res.status(200).json({
+      mensagem: 'Pet deletado com sucesso',
+    });
+  }
 }
--- src/routes/PetRoutes.ts
+++ src/routes/PetRoutes.ts
@@ -4,6 +4,11 @@ import PetController from '../controllers/PetController';
 const router = express.Router();
 const petController = new PetController();
 
-router.post('/pets', petController.criaPet);
+router
+  .post('/pets', petController.criaPet)
+  .get('/pets', petController.listaPet)
+  .get('/pets/:id', petController.listaPet)
+  .put('/pets/:id', petController.atualizaPet)
+  .delete('/pets/:id', petController.deletaPet);
 
 export default router;
